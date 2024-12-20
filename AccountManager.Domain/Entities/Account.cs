b1da50ba9667c5762ac32760b976903bceeb7188
Yudchits <pashka.yudchits@gmail.com>
2024-12-21 00:43:32 +0300

    Added entities

---
+++ AccountManager.Domain/Entities/Account.cs
@@ -0,0 +1,11 @@
+﻿using AccountManager.Domain.Common;
+
+namespace AccountManager.Domain.Entities
+{
+    public class Account : BaseEntity
+    {
+        public int ResourceId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ AccountManager.Domain/Entities/Resource.cs
@@ -0,0 +1,10 @@
+﻿using AccountManager.Domain.Common;
+
+namespace AccountManager.Domain.Entities
+{
+    public class Resource : BaseEntity
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
